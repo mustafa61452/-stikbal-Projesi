1d4fe9060a6a9da122f80ac7f6db342053fcadf3
mustafa61452 <mdfa61@gmail.com>
2026-01-31 22:55:40 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'istikbal-archive-v9';
+const CACHE_NAME = 'istikbal-archive-v10';
 const ASSETS = [
   './',
   './index.html',
   './app-v2.js',
   './styles.css',
-  './assets/icons/icon-192-v3.png',
-  './assets/icons/icon-512-v3.png',
+  './assets/icons/icon-192-v4.png',
+  './assets/icons/icon-512-v4.png',
   'https://cdn.tailwindcss.com',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
 ];
